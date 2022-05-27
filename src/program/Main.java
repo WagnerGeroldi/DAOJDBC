162bf8a8fc06f698806b2cd429c14fcfdcf6d95f
WagnerGeroldi <wagnergeroldi@gmail.com>
2022-05-27 15:56:34 -0300

    add class department

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,9 +1,16 @@
 package program;
 
+import model.entities.Department;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Department dp = new Department(1, "Livros");
+		
+		
+		
+		System.out.println(dp.toString());
 
 	}
 
