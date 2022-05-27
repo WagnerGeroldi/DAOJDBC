114d27fc4772a1f68176e623e2df9561b1bdc75c
WagnerGeroldi <wagnergeroldi@gmail.com>
2022-05-27 19:03:41 -0300

    add findById

---
+++ src/model/dao/implement/SellerDaoJDBC.java
@@ -0,0 +1,87 @@
+package model.dao.implement;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	private Connection connection;
+
+	public SellerDaoJDBC(Connection connection) {
+		this.connection = connection;
+	}
+
+	@Override
+	public void insert(Seller department) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Seller department) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+
+			preparedStatement = connection
+					.prepareStatement("SELECT seller.*, department.* " + "FROM seller INNER JOIN department "
+							+ "ON seller.department_id = department.id_department " + "WHERE seller.id_seller = ?");
+
+			preparedStatement.setInt(1, id);
+
+			resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+				Department dep = new Department();
+				dep.setId(resultSet.getInt("id_department"));
+				dep.setName(resultSet.getString("name_department"));
+				Seller seller = new Seller();
+				seller.setId(resultSet.getInt("id_seller"));
+				seller.setName(resultSet.getString("name"));
+				seller.setBirthDate(resultSet.getDate("birthDate"));
+				seller.setEmail(resultSet.getString("email"));
+				seller.setBaseSalary(resultSet.getDouble("baseSalary"));
+				seller.setDepartment(dep);
+
+				return seller;
+
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+			DB.closeResultSet(resultSet);
+		}
+
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/program/Main.java
+++ src/program/Main.java
@@ -1,22 +1,23 @@
 package program;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Livros");
-		Seller seller = new Seller(21, "wag", "wagner@gmail.com", new Date(), 1500.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
+		Seller seller = sellerDao.findById(3);
 		
-		System.out.println(dp.toString());
-		System.out.println(seller.toString());
-
+		
+		System.out.println(seller);
+		
+		
 	}
 
 }
